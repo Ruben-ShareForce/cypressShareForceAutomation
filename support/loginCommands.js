e5e601a840f0487b1ddc477153e223e841c78216
Ruben Da Silva <rubend@shareforce.net>
2026-01-08 16:30:43 +0200

    Added functionality to navigate to mailpit

--- support/loginCommands.js
+++ support/loginCommands.js
@@ -1,17 +1,41 @@
 Cypress.Commands.add("attemptLoginWithWrongPassword", (opts = {}) => {
   const {
-    domain = "example.invalid",          // .invalid is safe for fake emails
+    domain = "example.invalid",
     password = "wrongpassword",
     emailPrefix = "cypress-invalid",
   } = opts;
 
   const unique = `${Date.now()}-${Cypress._.random(1000, 9999)}`;
-  const email = `${emailPrefix}+${unique}@${domain}`;
+  const email = `${emailPrefix}${unique}@${domain}`;
 
-  cy.get("#id_auth-username").clear().type(email);
+  cy.get("#id_auth-username")
+    .clear()
+    .type(email, { parseSpecialCharSequences: false });
+
   cy.get("#auth_step_button").click();
 
-  cy.get("#id_auth-password").clear().type(password, { log: false });
+  cy.get("#id_auth-password")
+    .clear()
+    .type(password, { log: false, parseSpecialCharSequences: false });
+
+  cy.get("#auth_step_button").click();
+
+  return cy.wrap(email, { log: false });
+});
+
+Cypress.Commands.add("attemptForgotPasswordWithFakeEmail", (opts = {}) => {
+  const {
+    domain = "example.invalid",
+    emailPrefix = "cypress-invalid",
+  } = opts;
+
+  const unique = `${Date.now()}-${Cypress._.random(1000, 9999)}`;
+  const email = `${emailPrefix}${unique}@${domain}`;
+
+  cy.get("#id_auth-username")
+    .clear()
+    .type(email, { parseSpecialCharSequences: false });
+
   cy.get("#auth_step_button").click();
 
   return cy.wrap(email, { log: false });
