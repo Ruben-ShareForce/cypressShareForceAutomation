590d207f726d0965da85cafd758c2a0cdac07961
Ruben Da Silva <rubend@shareforce.net>
2026-01-08 08:19:40 +0200

    Added 1 more validation to login

--- e2e/ShareForce/Login & Logout/login.cy.js
+++ e2e/ShareForce/Login & Logout/login.cy.js
@@ -1,35 +1,55 @@
 describe("ShareForce Login", () => {
     it("checks the validations for the initial login process", () => {
         cy.visit("/");
-        cy.wait(500);
+        cy.wait(1000);
 
         cy.url().should("include", "/login");
-    
+        cy.wait(500);
+
         cy.get('#djHideToolBarButton').then(($btn) => {
             if ($btn.is(':visible')) {
                 cy.get('#djHideToolBarButton').click();
             }
         });
+        cy.wait(500);
 
+        // required field validation
         cy.get("#auth_step_button").click();
 
         const requiredValidations = [
             { name: "id_auth-username", message: "Please enter your email address" }
         ];
 
-       requiredValidations.forEach(({ name, message }) => {
+        requiredValidations.forEach(({ name, message }) => {
             cy.get(`p[for='${name}'].error-red`)
                 .filter(':visible')
                 .first()
                 .should('contain.text', message);
-            });
-    })
-    it("logs onto the ShareForce Demo app", () => {
+        });
+        cy.wait(500);
+
+        // minimum format validation
+        cy.get("#id_auth-username").type("d");
+        cy.wait(500);
+
+        const formatValidations = [
+            { name: "id_auth-username", message: "Your email address is not formatted correctly" }
+        ];
+
+        formatValidations.forEach(({ name, message }) => {
+            cy.get(`p[for='${name}'].error-red`)
+                .filter(':visible')
+                .first()
+                .should('contain.text', message);
+        });
+    });
+
+    it.skip("logs onto the ShareForce Demo app", () => {
         cy.visit("/");
         cy.wait(500);
 
         cy.url().should("include", "/login");
-    
+
         cy.get('#djHideToolBarButton').then(($btn) => {
             if ($btn.is(':visible')) {
                 cy.get('#djHideToolBarButton').click();
@@ -45,6 +65,5 @@ describe("ShareForce Login", () => {
         cy.wait(500);
         cy.get("#auth_step_button").click();
         cy.wait(500);
-    })
-
-})
+    });
+});
