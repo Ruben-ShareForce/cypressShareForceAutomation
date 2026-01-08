f2272beb55bc543f7a94512d501ae4302db1bf19
Ruben Da Silva <rubend@shareforce.net>
2026-01-08 10:26:36 +0200

    Added logout test case

---
+++ e2e/ShareForce Admin Portal/Login & Logout/logout.cy.js
@@ -0,0 +1,41 @@
+describe("ShareForce Logout", () => {
+    it("logs in and out of the ShareForce app", () => {
+            cy.visit("/");
+            cy.wait(500);
+
+            cy.url().should("include", "/login");
+
+            cy.get("#djHideToolBarButton").then(($btn) => {
+                if ($btn.is(":visible")) {
+                    cy.get("#djHideToolBarButton").click();
+                }
+            });
+
+            cy.get("#id_auth-username").type("dev@shareforce.co.za");
+            cy.wait(500);
+            cy.get("#auth_step_button").click();
+            cy.wait(500);
+
+            cy.get("#id_auth-password").type("ShareForce360!", { log: false });
+            cy.wait(500);
+            cy.get("#auth_step_button").click();
+            cy.wait(2000);
+
+            cy.url().should("include", "/app");
+
+            cy.reload();
+
+            cy.get("a.account-link.account-options.dropdown-toggle").should("be.visible").click();
+            cy.wait(500);
+
+            cy.get("a").contains("Logout").should("be.visible").click();
+            cy.wait(500);
+
+            cy.contains("p.mb20", "Are you sure you want to logout of your account?").should("be.visible");
+            cy.wait(500);
+
+            cy.get("a.btn.btn-sf-lightblue").should("be.visible").click();
+            cy.wait(1000);
+            
+        });
+});
