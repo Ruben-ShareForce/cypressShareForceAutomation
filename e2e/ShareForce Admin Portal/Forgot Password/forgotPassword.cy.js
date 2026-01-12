b321153b068418122991888ad82f9a66b151681a
Ruben Da Silva <rubend@shareforce.net>
2026-01-12 09:29:45 +0200

    Resetting the DOM to find the new url

--- e2e/ShareForce Admin Portal/Forgot Password/forgotPassword.cy.js
+++ e2e/ShareForce Admin Portal/Forgot Password/forgotPassword.cy.js
@@ -36,7 +36,7 @@ describe("Forgout Password Flow" , () => {
         cy.wait(500);
 
         cy.get("button.btn.bg-secondary.text-white.text-uppercase").should("be.visible").click();
-        cy.wait(500);
+        cy.wait(1000);
 
         cy.get("h1.text-white.open-sans-reg").should("be.visible").contains("Thanks!");
         cy.get("p.text-white.mb-2.opensans-normal").should("be.visible").contains("We have emailed you instructions for setting your password, if an account exists with the email you entered. You should receive them shortly.");
@@ -58,25 +58,29 @@ describe("Forgout Password Flow" , () => {
         cy.wait(500);
 
         cy.get('a[href*="/reset/"]')
-            .first()
-            .invoke('attr', 'href')
-            .then((resetUrl) => {
-                // Convert https://127.0.0.1:8000/... -> http://127.0.0.1:8000/...
-                const httpUrl = resetUrl.replace(/^https:\/\//, "http://");
+        .first()
+        .invoke('attr', 'href')
+        .then((resetUrl) => {
+            const httpUrl = resetUrl.replace(/^https:\/\//, "http://");
 
-                cy.visit(httpUrl);
-            });
-        
-        cy.wait(1500);
+            cy.visit(httpUrl);
 
-        cy.origin("http://127.0.0.1:8000", () => {
-        cy.get("#djHideToolBarButton").then(($btn) => {
-            if ($btn.is(":visible")) {
-            cy.get("#djHideToolBarButton").click();
+            cy.origin("http://127.0.0.1:8000", () => {
+            cy.get("#djHideToolBarButton").then(($btn) => {
+                if ($btn.is(":visible")) {
+                cy.wrap($btn).click();
                 }
-             });
+            });
+
+            cy.get("h1.text-white.open-sans-reg", { timeout: 10000 })
+                .should("be.visible")
+                .and("contain.text", "Set your Password");
+
+            cy.get("p.text-white.mb-2.opensans-normal", { timeout: 10000 })
+                .should("be.visible")
+                .and("contain.text", "Please set your new password.");
+            });
         });
 
-        cy.wait(500);
     });
 });
