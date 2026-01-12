df25bafc757578287d3fda50b151e4958bf6527e
Ruben Da Silva <rubend@shareforce.net>
2026-01-12 16:34:34 +0200

    Navigation before validation checks

--- e2e/ShareForce Admin Portal/Employee Manager/Employee Database/addEmployee.cy.js
+++ e2e/ShareForce Admin Portal/Employee Manager/Employee Database/addEmployee.cy.js
@@ -7,6 +7,27 @@ describe("Add Employee Flow" , () => {
     it("can verify all validations for adding a new employee", () => {
         cy.visit("/");
         cy.wait(500);
+
+        cy.get("a[href='/app/employees/']").should("be.visible").click();
+        cy.wait(500);
+
+        cy.get("a[href='/app/employees/create/']").should("be.visible").click();
+        cy.wait(1000);
+
+        // cancel logic here
+        cy.get("a[href='/app/employees/']").first().scrollIntoView().should("be.visible").click();
+        cy.wait(1000);
+        // end of cancel logic
+
+        cy.get("a[href='/app/employees/create/']").should("be.visible").click();
+        cy.wait(2000);
+
+        // click add button
+        cy.contains("button", "Create Employee")
+            .scrollIntoView()
+            .should("be.visible")
+            .click();
+
     });
 
 });
