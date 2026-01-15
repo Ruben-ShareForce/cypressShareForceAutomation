0edaac0abeb6eb790f410bdd341955715c01474e
Ruben Da Silva <rubend@shareforce.net>
2026-01-15 10:18:41 +0200

    Working Employee creation

--- cypress.config.js
+++ cypress.config.js
@@ -7,42 +7,30 @@ module.exports = defineConfig({
   projectId: "ffoabf",
   e2e: {
     specPattern: "e2e/**/*.cy.js",
+    fixturesFolder: "fixtures",
     supportFile: "support/e2e.js",
     viewportWidth: 1920,
     viewportHeight: 1080,
     baseUrl: "http://127.0.0.1:8000/",
-    
+
     setupNodeEvents(on, config) {
       on("task", {
         generateEmployee() {
           const name = faker.person.firstName();
           const surname = faker.person.lastName();
-
           const employee_id = `CYP${Date.now()}${faker.number.int({ min: 100, max: 999 })}`;
-
-          const email = faker.internet
-            .email({ firstName: name, lastName: surname })
-            .toLowerCase();
-
+          const email = faker.internet.email({ firstName: name, lastName: surname }).toLowerCase();
           return { name, surname, employee_id, email };
         },
 
-        saveEmployeeFixture(employee) {
-          const fixturesDir = path.join(config.projectRoot, "cypress", "fixtures");
-          const filePath = path.join(fixturesDir, "employee.json");
-
-          if (!fs.existsSync(fixturesDir)) {
-            fs.mkdirSync(fixturesDir, { recursive: true });
-          }
-
-          fs.writeFileSync(filePath, JSON.stringify(employee, null, 2), "utf8");
-          return filePath; // return something useful for debugging
+        ensureDir({ dir }) {
+          const abs = path.isAbsolute(dir) ? dir : path.join(config.projectRoot, dir);
+          fs.mkdirSync(abs, { recursive: true });
+          return abs;
         },
       });
 
       return config;
     },
-
-    // you need to have your localhost server running before executing the tests
   },
 });
--- e2e/ShareForce Admin Portal/Employee Manager/Employee Database/addEmployee.cy.js
+++ e2e/ShareForce Admin Portal/Employee Manager/Employee Database/addEmployee.cy.js
@@ -1,146 +1,106 @@
-describe("Add Employee Flow" , () => {
+let employeeFixture;
 
-    let employeeFixture;
+describe("Add Employee Flow", () => {
 
-    before(() => {
-        cy.generateEmployeeFixture().then((employee) => {
+  before(() => {
+    cy.generateEmployeeFixture().then((employee) => {
         employeeFixture = employee;
-
         cy.log(`Generated Employee: ${JSON.stringify(employeeFixture)}`);
         });
     });
-    
-    beforeEach(() => {
-        cy.loginRoot();
-        cy.wait(1000);
+
+  beforeEach(() => {
+    cy.loginRoot();
+    cy.wait(1000);
+  });
+
+  it("can verify all validations for adding a new employee", () => {
+    cy.visit("/");
+    cy.wait(500);
+
+    cy.get("body").then(($body) => {
+      if ($body.find("#djHideToolBarButton:visible").length) {
+        cy.get("#djHideToolBarButton").click();
+      }
     });
-    
-    it.skip("can verify all validations for adding a new employee", () => {
-        cy.visit("/");
-        cy.wait(500);
+    cy.wait(500);
 
-        cy.get('#djHideToolBarButton').then(($btn) => {
-            if ($btn.is(':visible')) {
-                cy.get('#djHideToolBarButton').click();
-            }
-        });
-        cy.wait(500);
+    cy.get("a[href='/app/employees/']").should("be.visible").click();
+    cy.wait(500);
 
-        cy.get("a[href='/app/employees/']").should("be.visible").click();
-        cy.wait(500);
+    cy.get("a[href='/app/employees/create/']").should("be.visible").click();
+    cy.wait(1000);
 
-        cy.get("a[href='/app/employees/create/']").should("be.visible").click();
-        cy.wait(1000);
+    cy.contains("button", "Create Employee")
+      .scrollIntoView()
+      .should("be.visible")
+      .click();
 
-        // cancel logic here
-        cy.get("a[href='/app/employees/']").first().scrollIntoView().should("be.visible").click();
-        cy.wait(1000);
-        // end of cancel logic
+    // Required field validations
+    const requiredFields = [
+      { name: "name", message: "Please enter your name" },
+      { name: "surname", message: "Please enter your surname" },
+      { name: "employee_id", message: "Please enter your employee ID" },
+    ];
 
-        cy.get("a[href='/app/employees/create/']").should("be.visible").click();
-        cy.wait(4000);
-
-        // click add button to trigger validations
-        cy.contains("button", "Create Employee")
-            .scrollIntoView()
-            .should("be.visible")
-            .click();
-
-        // required validations
-        const requiredFields= [
-            { name: "name", message: "Please enter your name" },
-            { name: "surname", message: "Please enter your surname" },
-            { name: "employee_id", message: "Please enter your employee ID" },
-        ];
-
-       requiredFields.forEach(({ name, message }) => {
-            const domId = name.startsWith("id_") ? name : `id_${name}`;
-
-            cy.get(`em[for='${domId}'].error-state`)
-                .should('be.visible')
-                .and('contain.text', message);
-        });
-
-        cy.wait(500);
-
-        // removing required field validations by filling in data
-        cy.get("#id_name").type("Cypress");
-        cy.get("#id_surname").type("Test");
-        cy.get("#id_employee_id").type(`CYP${Date.now()}`);
-
-        cy.contains("button", "Create Employee")
-            .scrollIntoView()
-            .should("be.visible")
-            .click();
-
-        cy.wait(2500);
-
-        // having to trigger email validation manually afterwards
-        const globalValidations = [
-            { message: "Please complete the email address or select 'User has no email'" },
-        ];
-
-        globalValidations.forEach(({ message }) => {
-            cy.contains(".alert.alert-danger li",
-                "Please complete the email address or select 'User has no email'").should("be.visible");
-        });
-
-
-        cy.generateEmployeeFixture().then((employee) => {
-            cy.get("#id_name").clear().type(employee.name);
-            cy.get("#id_surname").clear().type(employee.surname);
-            cy.get("#id_employee_id").clear().type(employee.employee_id);
-            cy.get("#id_email").clear().type(employee.email, { parseSpecialCharSequences: false });
-        });
-
-        cy.wait(500);
-        
-        cy.contains("button", "Create Employee").scrollIntoView().should("be.visible").click();
-        cy.wait(1000);
-
-        cy.contains("a", "Delete Employee")
-            .should("be.visible")
-            .click();
-
-        cy.wait(500);
-
-        cy.get("span.btn.btn-sf-lightblue.confirm").should("be.visible").first().click();
-
+    requiredFields.forEach(({ name, message }) => {
+      const domId = `id_${name}`;
+      cy.get(`em[for='${domId}'].error-state`)
+        .should("be.visible")
+        .and("contain.text", message);
     });
 
-    it("can add a new employee successfully", () => {
-        cy.visit("/");
-        cy.wait(500);
+    cy.wait(500);
 
-        cy.get('#djHideToolBarButton').then(($btn) => {
-            if ($btn.is(':visible')) {
-                cy.get('#djHideToolBarButton').click();
-            }
-        });
-        cy.wait(500);
+    // Fill required fields except email
+    cy.get("#id_name").clear().type("Cypress");
+    cy.get("#id_surname").clear().type("Test");
+    cy.get("#id_employee_id").clear().type(`CYP${Date.now()}`);
 
-        cy.get("a[href='/app/employees/']").should("be.visible").click();
-        cy.wait(500);
+    cy.contains("button", "Create Employee")
+      .scrollIntoView()
+      .should("be.visible")
+      .click();
 
-        cy.get("a[href='/app/employees/create/']").should("be.visible").click();
-        cy.wait(1000);
+    cy.wait(2500);
 
-        cy.generateEmployeeFixture().then((employee) => {
-            cy.get("#id_name").clear().type(employee.name);
-            cy.get("#id_surname").clear().type(employee.surname);
-            cy.get("#id_employee_id").clear().type(employee.employee_id);
-            cy.get("#id_email").clear().type(employee.email, { parseSpecialCharSequences: false });
-        });
+    // Email validation (top-level alert)
+    cy.contains(
+      ".alert.alert-danger li",
+      "Please complete the email address or select 'User has no email'"
+    ).should("be.visible");
 
-        cy.wait(500);
-        
-        // we could add all fields for an employee but it would be a lot of overhead due to the amount of fields
+    cy.get("#id_name").clear().type(employeeFixture.name);
+    cy.get("#id_surname").clear().type(employeeFixture.surname);
+    cy.get("#id_employee_id").clear().type(employeeFixture.employee_id);
+    cy.get("#id_email")
+      .clear()
+      .type(employeeFixture.email, { parseSpecialCharSequences: false });
 
-        cy.contains("button", "Create Employee").scrollIntoView().should("be.visible").click();
-        cy.wait(1000);
+    cy.wait(500);
 
-        
+    cy.contains("button", "Create Employee")
+      .scrollIntoView()
+      .should("be.visible")
+      .click();
 
+    cy.url().should("include", "/app/employees/detail/");
+    cy.wait(1000);
+
+    cy.contains("a", "Delete Employee")
+      .should("be.visible")
+      .click();
+
+    cy.wait(500);
+
+    cy.get("#confirmation-modal:visible").within(() => {
+      cy.contains("span.confirm", "Yes")
+        .should("be.visible")
+        .click();
     });
+
+    cy.wait(1000);
+    cy.url().should("include", "/app/employees/");
+  });
 
 });
