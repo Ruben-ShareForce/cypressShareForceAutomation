7e921beaf0c5345e2a928800afc5c65cdcd79650
Ruben Da Silva <rubend@shareforce.net>
2026-01-15 11:41:06 +0200

    Added Employee Activities export

---
+++ e2e/ShareForce Admin Portal/Employee Manager/Employee Database/exportEmployeeActivities.cy.js
@@ -0,0 +1,36 @@
+describe("Export Employee Activities", () => {
+
+    beforeEach(() => {
+        cy.loginRoot();
+        cy.wait(1000);
+    });
+
+    it("can export employee activities successfully", () => {
+        cy.visit("/");
+        cy.wait(500);
+
+        cy.get("body").then(($body) => {
+            if ($body.find("#djHideToolBarButton:visible").length) {
+                cy.get("#djHideToolBarButton").click();
+            }
+        });
+        cy.wait(500);
+
+        cy.get("a[href='/app/employees/']").should("be.visible").click();
+        cy.wait(500);
+
+        cy.get("button.btn.btn-sf-default.dropdown-toggle[data-toggle='dropdown']")
+          .should("be.visible").contains("Export Data").click();
+
+        cy.get("a.bg-file-trigger[data-url='/app/employees/export/activities/?']")
+            .should("be.visible").click();
+
+        cy.wait(2000);
+        
+        const downloadPath = 'cypress/downloads/EmployeeActivities.xlsx';
+        cy.wait(2500);
+        cy.readFile(downloadPath, { timeout: 15000 }).should((content) => {
+            expect(content).to.not.be.empty;
+        });
+    });
+});
