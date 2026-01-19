7bed9b2610eb8bdb551c6508c83d82c5e06449f4
Ruben Da Silva <rubend@shareforce.net>
2026-01-19 15:55:36 +0200

    Working version of exporting payroll transfer history

---
+++ e2e/ShareForce Admin Portal/Employee Manager/Employee Database/Export Data/exportPayrollTransferHistory.cy.js
@@ -0,0 +1,31 @@
+describe("Export Payroll Transfer History", () => {
+ 
+    beforeEach(() => {
+        cy.loginRoot();
+        cy.wait(1000);
+    });
+ 
+    it("can export payroll transfer history successfully", () => {
+        cy.visit("/");
+        cy.wait(500);
+ 
+        cy.get("body").then(($body) => {
+            if ($body.find("#djHideToolBarButton:visible").length) {
+                cy.get("#djHideToolBarButton").click();
+            }
+        });
+        cy.wait(500);
+ 
+        cy.get("a[href='/app/employees/']").should("be.visible").click();
+        cy.wait(500);
+ 
+        cy.get("button.btn.btn-sf-default.dropdown-toggle[data-toggle='dropdown']")
+          .should("be.visible").contains("Export Data").click();
+ 
+        cy.get("a.bg-file-trigger[data-url='/app/employees/hr/payrolls/history/export/?']")
+            .should("be.visible").click();
+ 
+        cy.wait(2000);
+       
+    });
+});
