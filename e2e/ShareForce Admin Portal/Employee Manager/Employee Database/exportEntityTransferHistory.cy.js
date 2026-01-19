76ae2b3a686b30a597d61c3eab029400652dfb6b
Ruben Da Silva <rubend@shareforce.net>
2026-01-19 14:28:52 +0200

    Fixed exporter logic

--- e2e/ShareForce Admin Portal/Employee Manager/Employee Database/exportEntityTransferHistory.cy.js
+++ e2e/ShareForce Admin Portal/Employee Manager/Employee Database/exportEntityTransferHistory.cy.js
@@ -7,53 +7,37 @@ describe("Export Entity Transfer History", () => {
 
     it("can export employee entity transfer history successfully", () => {
         cy.visit("/");
-        cy.wait(500);
 
         cy.get("body").then(($body) => {
             if ($body.find("#djHideToolBarButton:visible").length) {
-                cy.get("#djHideToolBarButton").click();
+            cy.get("#djHideToolBarButton").click();
             }
         });
-        cy.wait(500);
 
         cy.get("a[href='/app/employees/']").should("be.visible").click();
-        cy.wait(500);
 
         cy.get("button.btn.btn-sf-default.dropdown-toggle[data-toggle='dropdown']")
-          .should("be.visible").contains("Export Data").click();
-
-        cy.get("a.bg-file-trigger[data-url='/app/employees/export/transfers/?']")
-            .should("be.visible").click();
-
-        cy.wait(2000);
-        
-    it("can export employee entity transfer history successfully", () => {
-        cy.loginRoot();
-
-        cy.intercept("GET", "/app/employees/export/transfers/?").as("export");
-
-        cy.visit("/app/employees/");
-
-        cy.get("button.btn.btn-sf-default.dropdown-toggle")
             .contains("Export Data")
+            .should("be.visible")
             .click();
 
-        cy.get("a.bg-file-trigger[data-url='/app/employees/export/transfers/?']")
+        cy.intercept("GET", "**/app/employees/export/transfers/**").as("startExport");
+
+        cy.get("a.bg-file-trigger[data-url^='/app/employees/export/transfers/']")
+            .should("be.visible")
             .click();
 
-        cy.wait("@export").then(({ response }) => {
+        cy.wait("@startExport").then(({ response }) => {
             expect(response.statusCode).to.eq(200);
 
-            expect(response.body).to.exist;
-            expect(response.body.length).to.be.greaterThan(1000);
-
-            expect(response.headers["content-type"])
-            .to.include("spreadsheet");
-
-            expect(response.headers["content-disposition"])
-            .to.include("attachment");
         });
-    });
 
-    });
+        const downloadsFolder = Cypress.config("downloadsFolder");
+        const filePath = `${downloadsFolder}/TransferHistory.xlsx`;
+
+        cy.readFile(filePath, "binary", { timeout: 30000 }).should((content) => {
+            expect(content.length).to.be.greaterThan(1000);
+        });
+        });
+
 });
