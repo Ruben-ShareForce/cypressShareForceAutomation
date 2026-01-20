7d0140355a74115c72857c92ecb749706d7b72fb
Ruben Da Silva <rubend@shareforce.net>
2026-01-20 13:58:49 +0200

    Filtering for is_leaver  working

--- e2e/ShareForce Admin Portal/Employee Manager/Employee Database/filterEmployee.cy.js
+++ e2e/ShareForce Admin Portal/Employee Manager/Employee Database/filterEmployee.cy.js
@@ -24,7 +24,7 @@ describe("Filter Employee Flow", () => {
 
     });
 
-  it("can filter for existing employee/s using a random entity value", () => {
+  it.skip("can filter for existing employee/s using a random entity value", () => {
       cy.visit("/");
 
       cy.get("body").then(($body) => {
@@ -47,6 +47,38 @@ describe("Filter Employee Flow", () => {
 
             const randomValue = values[Math.floor(Math.random() * values.length)];
             cy.get("#id_entity").select(randomValue);
+        });
+
+      cy.wait(500);
+      cy.get("button[type='submit'][class='btn btn-sf-lightblue pull_right']")
+        .should("be.visible")
+        .click();
+
+    });
+
+    it("can filter for existing employee/s using a random leavers (true/false) value", () => {
+      cy.visit("/");
+
+      cy.get("body").then(($body) => {
+        if ($body.find("#djHideToolBarButton:visible").length) {
+          cy.get("#djHideToolBarButton").click();
+        }
+      });
+
+      cy.get("a[href='/app/employees/']").should("be.visible").click();
+      cy.wait(1000);
+
+      cy.get("button[data-toggle='collapse'][data-target='#collapseOne']").should("be.visible").click();
+      cy.wait(1000);
+
+      cy.get("#id_leavers")
+        .should("be.visible")
+        .find("option")
+        .then(($options) => {
+            const values = [...$options].map(o => o.value);
+
+            const randomValue = values[Math.floor(Math.random() * values.length)];
+            cy.get("#id_leavers").select(randomValue);
         });
 
       cy.wait(500);
