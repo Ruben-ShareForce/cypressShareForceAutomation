541424dba83a33e7d582cda153d1758970d466ee
Ruben Da Silva <rubend@shareforce.net>
2026-01-20 13:53:07 +0200

    Filtering for entity working

---
+++ e2e/ShareForce Admin Portal/Employee Manager/Employee Database/filterEmployee.cy.js
@@ -0,0 +1,58 @@
+describe("Filter Employee Flow", () => {
+  beforeEach(() => {
+    cy.loginRoot();
+  });
+
+  it.skip("can toggle the filter panel on and off", () => {
+    cy.visit("/");
+    
+    cy.get("body").then(($body) => {
+      if ($body.find("#djHideToolBarButton:visible").length) {
+        cy.get("#djHideToolBarButton").click();
+        }
+    });
+    
+    cy.get("a[href='/app/employees/']").should("be.visible").click();
+    cy.wait(1000);
+    cy.get("button[data-toggle='collapse'][data-target='#collapseOne']").should("be.visible").click();
+    cy.wait(1000);
+
+    cy.get("h2.normalize").should("be.visible");
+    cy.wait(1000);
+
+    cy.get("input[type='button'][class='btn btn-sf-lightblue pull_right'][onclick=\"location.href='/app/employees/';\"]").should("be.visible");
+
+    });
+
+  it("can filter for existing employee/s using a random entity value", () => {
+      cy.visit("/");
+
+      cy.get("body").then(($body) => {
+        if ($body.find("#djHideToolBarButton:visible").length) {
+          cy.get("#djHideToolBarButton").click();
+        }
+      });
+
+      cy.get("a[href='/app/employees/']").should("be.visible").click();
+      cy.wait(1000);
+
+      cy.get("button[data-toggle='collapse'][data-target='#collapseOne']").should("be.visible").click();
+      cy.wait(1000);
+
+      cy.get("#id_entity")
+        .should("be.visible")
+        .find("option")
+        .then(($options) => {
+            const values = [...$options].map(o => o.value);
+
+            const randomValue = values[Math.floor(Math.random() * values.length)];
+            cy.get("#id_entity").select(randomValue);
+        });
+
+      cy.wait(500);
+      cy.get("button[type='submit'][class='btn btn-sf-lightblue pull_right']")
+        .should("be.visible")
+        .click();
+
+    });
+  });
