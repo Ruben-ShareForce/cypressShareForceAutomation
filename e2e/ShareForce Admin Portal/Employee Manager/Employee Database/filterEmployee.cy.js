4b49cda712f7d5946b5e65dee8d08eacd0fc44fe
Ruben Da Silva <rubend@shareforce.net>
2026-01-20 15:38:00 +0200

    Filtering for employees using all filters in simulation

--- e2e/ShareForce Admin Portal/Employee Manager/Employee Database/filterEmployee.cy.js
+++ e2e/ShareForce Admin Portal/Employee Manager/Employee Database/filterEmployee.cy.js
@@ -3,7 +3,7 @@ describe("Filter Employee Flow", () => {
     cy.loginRoot();
   });
 
-  it("can toggle the filter panel on and off", () => {
+  it.skip("can toggle the filter panel on and off", () => {
     cy.visit("/");
     
     cy.get("body").then(($body) => {
@@ -22,9 +22,9 @@ describe("Filter Employee Flow", () => {
 
     cy.get("input[type='button'][class='btn btn-sf-lightblue pull_right'][onclick=\"location.href='/app/employees/';\"]").should("be.visible");
     cy.wait(1000);
-    });
+  });
 
-  it("can filter for existing employee/s using a random entity value", () => {
+  it.skip("can filter for existing employee/s using a random entity value", () => {
       cy.visit("/");
 
       cy.get("body").then(($body) => {
@@ -56,7 +56,7 @@ describe("Filter Employee Flow", () => {
       cy.wait(1000);
     });
 
-    it("can filter for existing employee/s using a random leavers (true/false) value", () => {
+    it.skip("can filter for existing employee/s using a random leavers (true/false) value", () => {
       cy.visit("/");
 
       cy.get("body").then(($body) => {
@@ -88,7 +88,7 @@ describe("Filter Employee Flow", () => {
       cy.wait(1000);
     });
 
-    it("can filter for existing employee/s using a random leave reason value", () => {
+    it.skip("can filter for existing employee/s using a random leave reason value", () => {
       cy.visit("/");
 
       cy.get("body").then(($body) => {
@@ -120,7 +120,7 @@ describe("Filter Employee Flow", () => {
       cy.wait(1000);
     });
 
-    it("can filter for existing employee/s using a random account status value", () => {
+    it.skip("can filter for existing employee/s using a random account status value", () => {
       cy.visit("/");
 
       cy.get("body").then(($body) => {
@@ -152,7 +152,7 @@ describe("Filter Employee Flow", () => {
       cy.wait(1000);
     });
 
-    it("can filter for existing employee/s using a random users with email (true/false) value", () => {
+    it.skip("can filter for existing employee/s using a random users with email (true/false) value", () => {
       cy.visit("/");
 
       cy.get("body").then(($body) => {
@@ -184,7 +184,7 @@ describe("Filter Employee Flow", () => {
       cy.wait(1000);
     });
 
-    it("can filter for existing employee/s using a random is director (true/false) value", () => {
+    it.skip("can filter for existing employee/s using a random is director (true/false) value", () => {
       cy.visit("/");
 
       cy.get("body").then(($body) => {
@@ -216,7 +216,7 @@ describe("Filter Employee Flow", () => {
       cy.wait(1000);
     });
 
-    it("can filter for existing employee/s using a random has awards (true/false) value", () => {
+    it.skip("can filter for existing employee/s using a random has awards (true/false) value", () => {
       cy.visit("/");
 
       cy.get("body").then(($body) => {
@@ -248,4 +248,74 @@ describe("Filter Employee Flow", () => {
       cy.wait(1000);
     });
 
+    it("can filter for existing employee/s using a random combination of all filters", () => {
+      cy.visit("/");
+
+      cy.get("body").then(($body) => {
+        if ($body.find("#djHideToolBarButton:visible").length) {
+          cy.get("#djHideToolBarButton").click();
+        }
+      });
+      cy.get("a[href='/app/employees/']").should("be.visible").click();
+      cy.wait(1000);
+
+      cy.get("button[data-toggle='collapse'][data-target='#collapseOne']").should("be.visible").click();
+      cy.wait(1000);
+
+      const randomSelect = (selector, { includeEmpty = true } = {}) => {
+        cy.get(selector)
+          .should("be.visible")
+          .find("option")
+          .then(($options) => {
+            let values = [...$options].map(o => o.value);
+
+            if (!includeEmpty) values = values.filter(v => v !== "");
+
+            const randomValue = values[Math.floor(Math.random() * values.length)];
+
+            cy.get(selector).select(randomValue);
+            cy.log(`${selector} => ${randomValue === "" ? "(empty/default)" : randomValue}`);
+          });
+      };
+
+      const filters = [
+        "#id_entity",
+        "#id_leavers",
+        "#id_reason_for_leaving",
+        "#id_account_status",
+        "#id_no_email",
+        "#id_is_director",
+        "#id_has_awards",
+      ];
+
+      const runs = 3;
+
+      Cypress._.times(runs, (i) => {
+        cy.log(`Run ${i + 1}/${runs}`);
+
+        filters.forEach((sel) => randomSelect(sel, { includeEmpty: true }));
+
+        cy.wait(500);
+        cy.get("button[type='submit'][class='btn btn-sf-lightblue pull_right']").should("be.visible").click();
+        cy.wait(1000);
+
+        cy.get("body").then(($body) => {
+          if ($body.find("#list_table").length) {
+            cy.get("#list_table").should("be.visible");
+          } else {
+            cy.get(".sf-box-inner").should("be.visible");
+          }
+        });
+
+        cy.get(
+          "input[type='button'][class='btn btn-sf-lightblue pull_right'][onclick=\"location.href='/app/employees/';\"]"
+        )
+          .should("be.visible")
+          .click();
+        cy.wait(1000);
+
+        cy.get("button[data-toggle='collapse'][data-target='#collapseOne']").should("be.visible").click();
+        cy.wait(500);
+      });
+    });
 });
