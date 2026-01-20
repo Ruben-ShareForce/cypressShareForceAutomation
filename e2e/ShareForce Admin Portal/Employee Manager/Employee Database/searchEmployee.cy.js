6a7b3394331a273d84659cadd8d9c6b017be3a6a
Ruben Da Silva <rubend@shareforce.net>
2026-01-20 10:52:05 +0200

    Final version of search employee functionality

--- e2e/ShareForce Admin Portal/Employee Manager/Employee Database/searchEmployee.cy.js
+++ e2e/ShareForce Admin Portal/Employee Manager/Employee Database/searchEmployee.cy.js
@@ -32,4 +32,46 @@ describe("Search Employee Flow", () => {
       cy.wait(1000);
     });
   });
+
+  it("shows no results when searching for a non-existing employee", () => {
+        cy.visit("/");
+
+        cy.get("a[href='/app/employees/']").should("be.visible").click();
+        cy.wait(1000);
+
+        cy.get("input[name='q']").should("be.visible").type("nonexistent_employee", { delay: 50 }).type('{enter}');
+        cy.wait(1000);
+
+        cy.get(".sf-box-inner").should("be.visible").and("contain.text", "No employees matched your search query.");
+    });
+
+    it("can clear the search results for both an existing and non-existing employee", () => {
+        cy.visit("/");
+
+        cy.get("a[href='/app/employees/']").should("be.visible").click();
+        cy.wait(1000);
+
+        // Search for an existing employee
+        cy.get("input[name='q']").should("be.visible").type("Cypress", { delay: 50 }).type('{enter}');
+        cy.wait(1000);
+        cy.contains("tr", "Cypress Test").should("be.visible");
+        cy.wait(1000);
+        cy.contains("a[href='/app/employees/']", "Clear search results").should("be.visible").click();
+        cy.wait(1000);
+        
+        // Search for a non-existing employee
+        cy.get("input[name='q']").should("be.visible").type("nonexistent_employee", { delay: 50 }).type('{enter}');
+        cy.wait(1000);
+        cy.get(".sf-box-inner").should("be.visible").and("contain.text", "No employees matched your search query.");
+        cy.wait(1000);
+
+        cy.contains("a[href='/app/employees/']", "Clear search results").should("be.visible").click();
+        cy.wait(1000);
+
+        cy.get("#list_table tbody tr")
+            .its("length")
+            .should("be.greaterThan", 2);
+
+    });
+
 });
