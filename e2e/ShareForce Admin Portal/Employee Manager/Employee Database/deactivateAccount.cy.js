cc41d730cd649d95c2be0f39933a766caeb9c513
Ruben Da Silva <rubend@shareforce.net>
2026-01-27 14:48:38 +0200

    Deactivating activate accounts

---
+++ e2e/ShareForce Admin Portal/Employee Manager/Employee Database/deactivateAccount.cy.js
@@ -0,0 +1,52 @@
+describe("Deactivate Account", () => {
+  beforeEach(() => {
+    cy.loginRoot();
+  });
+
+  it("can filter for active employees and deactivate their account", () => {
+    cy.visit("/");
+
+    cy.get("body").then(($body) => {
+        if ($body.find("#djHideToolBarButton:visible").length) {
+          cy.get("#djHideToolBarButton").click();
+        }
+      });
+
+    cy.get("a[href='/app/employees/']").should("be.visible").click();
+    cy.wait(1000);
+
+    cy.get("button[data-toggle='collapse'][data-target='#collapseOne']").should("be.visible").click();
+    cy.wait(1000);
+
+    cy.get("#id_account_status")
+      .should("be.visible")
+      .select("True");
+
+    cy.wait(500);
+    cy.get("button[type='submit'][class='btn btn-sf-lightblue pull_right']")
+        .should("be.visible")
+        .click();
+    cy.wait(1000);
+
+    cy.get("#list_table").should("be.visible");
+
+    cy.get("#list_table tbody tr").eq(1).should("be.visible");
+    cy.wait(500);
+
+    cy.get("#list_table tbody tr").eq(1)
+        .within(() => {
+            cy.get("button.dropdown-toggle")
+            .should("be.visible")
+            .click();
+        });
+
+    cy.contains("a", "Deactivate Account").should("be.visible").click();
+    cy.wait(1000);
+
+    cy.contains("span", "Yes").should("be.visible").click();
+    cy.wait(500);
+
+    cy.get("div.alert.alert-dismissable.alert-info").should("be.visible").contains("The account has been deactivated");
+  });
+
+});
